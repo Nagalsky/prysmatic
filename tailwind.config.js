fc95605d82f06e239b9cb37b20dd03b2f23d17ac
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-09-06 10:34:33 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
