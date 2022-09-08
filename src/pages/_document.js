bc77da5b65eb6e1071075ef4719ab216d39d97e4
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-09-08 08:09:50 +0200

    dark theme header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className="text-base antialiased">
+      <body className="text-base antialiased dark:bg-body-gradient dark:text-semi-white dark:bg-semi-white text-dark-500">
         <Main />
         <NextScript />
       </body>
--- src/sections/layout.js
+++ src/sections/layout.js
@@ -1,19 +1,19 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import { useEffect } from 'react'
+// import { useRouter } from 'next/router'
 import Header from '../sections/header'
 import Footer from '../sections/footer'
 
 export default function Layout({ children, ...restProps }) {
-  const router = useRouter()
+  // const router = useRouter()
 
-  const routes = ['/']
+  // const routes = ['/']
 
-  const bodyClasses = ['bg-body-gradient', 'text-semi-white']
+  // const bodyClasses = ['bg-body-gradient', 'text-semi-white']
 
-  useEffect(() => {
-    routes.includes(router.pathname) &&
-      document.body.classList.add(...bodyClasses)
-  })
+  // useEffect(() => {
+  //   routes.includes(router.pathname) &&
+  //     document.body.classList.add(...bodyClasses)
+  // })
 
   return (
     <div className="min-h-screen flex flex-col" {...restProps}>
