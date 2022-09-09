1d38c422cfb8309dd55fb34c73c69049a94f032a
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-09-09 11:43:31 +0200

    test pagination

--- src/components/gallery-pagination.js
+++ src/components/gallery-pagination.js
@@ -8,8 +8,10 @@ export function GalleryPagination({
     <button
       {...restProps}
       className={`${
-        currentSlide === idx ? ' opacity-1 w-6' : ' opacity-[0.4] w-4'
-      } rounded-md h-2 bg-purple-500 dark:bg-yellow-500 transition origin-center`}
+        currentSlide === idx
+          ? 'opacity-1 w-6 rounded-md h-2 bg-purple-500 dark:bg-yellow-500 transition origin-center'
+          : 'opacity-[0.4] w-4 rounded-md h-2 bg-purple-500 dark:bg-yellow-500 transition origin-center'
+      }`}
     ></button>
   )
 }
