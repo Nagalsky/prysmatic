99e9fa7434db3c9192b9b0d333d1615ceb2a8333
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2022-09-09 09:40:19 +0200

    one more test

--- src/components/gallery-pagination.js
+++ src/components/gallery-pagination.js
@@ -1,11 +1,8 @@
-export function GalleryPagination({currentSlide, idx, ...restProps}) {
-  return (
-    <button
-      {...restProps}
-      className={
-        'rounded-md h-2 bg-purple-500 dark:bg-yellow-500 transition origin-center' +
-        (currentSlide === idx ? ' opacity-1 w-6' : ' opacity-[0.4] w-4')
-      }
-    ></button>
-  )
+export function GalleryPagination({
+  className,
+  currentSlide,
+  idx,
+  ...restProps
+}) {
+  return <button {...restProps} className={className}></button>
 }
--- src/sections/about-meet-team.js
+++ src/sections/about-meet-team.js
@@ -252,6 +252,11 @@ export default function AboutMeetTeam({...restProps}) {
                     }}
                     currentSlide={currentSlide}
                     idx={idx}
+                    className={`${
+                      currentSlide === idx
+                        ? ' opacity-1 w-6'
+                        : ' opacity-[0.4] w-4'
+                    } rounded-md h-2 bg-purple-500 dark:bg-yellow-500 transition origin-center`}
                   />
                 )
               })}
